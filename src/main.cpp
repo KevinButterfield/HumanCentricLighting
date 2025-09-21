d6c26bc06887ceff5b1825e767e301e70e110b8a
Kevin Butterfield <ksbutterfiel@gmail.com>
2025-09-21 14:29:34 -0400

    WiFi connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
 
 const int LED_PIN = 2;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+
+  delay(1000);
+
+  Serial.printf("Connecting to Wi-Fi %s...", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(WiFi.status());
+  }
+
+  Serial.println("\nWi-Fi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+  }
+
+  digitalWrite(LED_PIN, HIGH);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
 }
