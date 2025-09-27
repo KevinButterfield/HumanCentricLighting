d5265db6d4e2acfe46ad2e81b804cce6525c8017
Kevin Butterfield <ksbutterfiel@gmail.com>
2025-09-27 16:13:41 -0400

    Remove unnecessary delay

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,8 @@ void setup() {
   BlinkingLight::Initialize();
   InputModule::Initialize();
   OutputModule::Initialize();
-  timekeeping.begin();
-
-  delay(1000);
-
-  Serial.printf("Connecting to Wi-Fi %s...", WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  timekeeping.begin();
 }
 
 void loop() {
