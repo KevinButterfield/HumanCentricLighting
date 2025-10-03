7af7d3d03f82ab98ac479f4bfbb6dc30e8b60910
Kevin Butterfield <ksbutterfiel@gmail.com>
2025-10-02 20:49:25 -0400

    Chart with drag

---
+++ data/index.js
@@ -0,0 +1,71 @@
+const exampleData = [
+  { x: 0.0, y: 2700 },
+  { x: 0.5, y: 6500 },
+  { x: 1.0, y: 2700 },
+]
+
+function renderChart() {
+  const chartCanvas = document.getElementById('chart');
+
+  const chart = new Chart(chartCanvas, {
+    type: 'line',
+    plugins: [ChartDataLabels],
+    data: {
+      labels: exampleData.map(d => d.x),
+      datasets: [{
+        label: 'My First Dataset',
+        data: exampleData,
+        tension: 0.4,
+      }]
+    },
+    options: {
+      layout: {
+        padding: {
+          top: 50,
+          bottom: 10,
+        }
+      },
+      scales: {
+        x: {
+        },
+        y: {
+          ticks: {
+            display: false,
+          },
+          suggestedMin: 2700,
+          suggestedMax: 6500,
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+        },
+        datalabels: {
+          display: (context) => context.dataIndex > 0 && context.dataIndex < context.dataset.data.length - 1,
+          formatter: (value) => Math.round(value.y) + 'K',
+          align: 'top',
+          anchor: 'end',
+        },
+        dragData: {
+          onDrag: (_event, datasetIndex, index, value) => {
+            // Plainly setting a minimum doesn't floor the value during drag
+            if (value.y < 2700) {
+              chart.data.datasets[datasetIndex].data[index].y = 2700;
+              chart.update('none');
+              return false;
+            }
+          }
+        },
+      },
+    },
+  });
+}
+
+function main() {
+  renderChart();
+}
+
+document.addEventListener('DOMContentLoaded', main);
